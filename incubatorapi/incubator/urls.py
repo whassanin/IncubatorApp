0882a583615b30216b425dd3f339c538317f5ff8
waleed hassanin <w.hassanin@gmail.com>
2020-07-29 06:48:26 +0200

    update 29/7/2020

--- incubatorapi/incubator/urls.py
+++ incubatorapi/incubator/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^incubator$',views.readAllIncubator)
+    url(r'^incubator$',views.readAllIncubator),
+    url(r'^incubator/add$',views.addIncubator)
 ]
--- incubatorapi/incubator/views.py
+++ incubatorapi/incubator/views.py
@@ -14,3 +14,12 @@ def readAllIncubator(request):
     incubatorData = Incubator.objects.all()
     incubator_serializer_Data = IncubatorSerializer(incubatorData,many=True)
     return JsonResponse(incubator_serializer_Data.data,safe=False)
+
+@api_view(['POST'])
+def addIncubator(request):
+    incubatorData = JSONParser().parse(request)
+    incubator_serializer_Data = IncubatorSerializer(data=incubatorData)
+    if incubator_serializer_Data.is_valid():
+        incubator_serializer_Data.save()
+        return JsonResponse(incubator_serializer_Data,status=status.HTTP_201_CREATED)
+    return JsonResponse(incubator_serializer_Data.errors,status=status.HTTP_400_BAD_REQUEST)
